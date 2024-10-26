595c3e48b8d265e2c9eae4dda7af66d7017a839d
Erik <szaszco@gmail.com>
2024-10-27 01:07:31 +0200

    implemented window pop on gs visualizer

--- src/gui/windows/visualization/rasterization_window.py
+++ src/gui/windows/visualization/rasterization_window.py
@@ -3,7 +3,7 @@ import torch
 from PySide6 import QtCore
 from PySide6.QtGui import Qt
 from PySide6.QtWidgets import QVBoxLayout, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem, \
-    QGraphicsSceneMouseEvent
+    QGraphicsSceneMouseEvent, QDialog, QMainWindow, QWidget, QSizePolicy
 
 from gui.windows.visualization.fx.temporal_anit_aliasing import TemporalAntiAliasing
 from gui.windows.visualization.viewer_interface import ViewerInterface
@@ -17,6 +17,45 @@ class State:
     TRANSLATE = 2
     ROLL = 3
     ZOOM = 4
+
+
+class PopupWindow(QMainWindow):
+    def __init__(self, parent, camera):
+        super().__init__(parent)
+
+        self.camera = camera
+
+        self.setWindowTitle("3DGS visualizer")
+
+        self.layout = QVBoxLayout()
+        self.layout.setContentsMargins(0, 0, 0, 0)
+        self.layout.setSpacing(0)
+
+        central_widget = QWidget(self)
+        central_widget.setLayout(self.layout)
+        self.setCentralWidget(central_widget)
+
+        self.graphics_view = None
+
+    def set_graphics_view(self, graphics_view: QGraphicsView):
+        if self.graphics_view is not None:
+            self.layout.removeWidget(self.graphics_view)
+            self.graphics_view.setParent(None)
+
+        if graphics_view is not None:
+            self.graphics_view = graphics_view
+            self.layout.addWidget(self.graphics_view)
+            self.graphics_view.setParent(self)
+            self.graphics_view.setSizePolicy(QSizePolicy.Policy.Expanding, QSizePolicy.Policy.Expanding)
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+
+        new_width = self.width() - 20
+        new_height = self.height() - 20
+        self.camera.width = new_width
+        self.camera.height = new_height
+        self.graphics_view.setGeometry(0, 0, self.width(), self.height())
 
 
 # noinspection PyTypeChecker
@@ -58,6 +97,9 @@ class GaussianSplatWindow(ViewerInterface):
 
         self.init_ui()
 
+        self.is_embedded = True
+        self.popup_window = None
+
     def init_ui(self):
         self.layout = QVBoxLayout(self)
         self.layout.setContentsMargins(0, 0, 0, 0)
@@ -74,11 +116,9 @@ class GaussianSplatWindow(ViewerInterface):
         self.scene.mousePressEvent = self.mousePressEventScene
         self.scene.mouseMoveEvent = self.mouseMoveEventScene
         self.scene.mouseReleaseEvent = self.mouseReleaseEventScene
+        self.scene.wheelEvent = self.wheelEventScene
 
         self.set_background_color(self.background_color)
-
-    def resizeEvent(self, event):
-        super().resizeEvent(event)
 
     def set_background_color(self, rgb_array):
         r_255 = int(rgb_array[0] * 255)
@@ -127,8 +167,8 @@ class GaussianSplatWindow(ViewerInterface):
         self.state = State.NONE
         torch.cuda.empty_cache()
 
-    def wheelEvent(self, event):
-        delta = event.angleDelta().y()
+    def wheelEventScene(self, event):
+        delta = event.delta()
         self.camera.zoom(delta * self.zoom_factor, self.get_aabb)
 
     @property
@@ -161,7 +201,25 @@ class GaussianSplatWindow(ViewerInterface):
 
     # TODO: Implement if needed
     def on_embed_button_pressed(self):
-        pass
+        if not self.is_embedded:
+            # Move back to main window
+            if self.popup_window:
+                self.popup_window.set_graphics_view(None)
+                self.layout.addWidget(self.graphics_view)
+                self.graphics_view.setParent(self)
+                self.popup_window.close()
+                self.is_embedded = True
+                self.camera.height = self.height() - 20
+                self.camera.width = self.width() - 20
+        else:
+            # Create and show the pop-up visualizer window
+            if not self.popup_window:
+                self.popup_window = PopupWindow(self, self.camera)
+
+            self.popup_window.setGeometry(0, 0, self.camera.width + 20, self.camera.height + 20)
+            self.popup_window.set_graphics_view(self.graphics_view)
+            self.is_embedded = False
+            self.popup_window.show()
 
     def update_transform(self, transformation):
         if self.pc1 is None or self.pc2 is None:
